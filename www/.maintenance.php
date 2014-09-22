1ad5f37f5aac798844396c33cd849bfbd28e30bc
Igor Hlina <srigi@srigi.sk>
2014-09-22 20:49:27 +0200

    Cody fancy & cleanup of .maintenance.php

--- www/.maintenance.php
+++ www/.maintenance.php
@@ -3,11 +3,10 @@
 header('HTTP/1.1 503 Service Unavailable');
 header('Retry-After: 300'); // 5 minutes in seconds
 
-?>
-<!DOCTYPE html>
+?><!doctype html>
 <meta charset="utf-8">
 <meta name="robots" content="noindex">
-<meta name="generator" content="Nette Framework">
+<title>Site is temporarily down for maintenance</title>
 
 <style>
 	body { color: #333; background: white; width: 500px; margin: 100px auto }
@@ -15,12 +14,8 @@ header('Retry-After: 300'); // 5 minutes in seconds
 	p { font: 21px/1.5 Georgia,serif; margin: 1.5em 0 }
 </style>
 
-<title>Site is temporarily down for maintenance</title>
-
 <h1>We're Sorry</h1>
-
 <p>The site is temporarily down for maintenance. Please try again in a few minutes.</p>
-
 <?php
 
 exit;

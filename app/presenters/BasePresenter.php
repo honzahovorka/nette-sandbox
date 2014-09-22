df265abb2b7942acb1b402ad1ca0d862c4eefeb4
Igor Hlina <srigi@srigi.sk>
2014-09-22 21:10:01 +0200

    Some useful configuration directives for application
    Also configure Presenters by DiC

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,4 +8,15 @@ use Nette\Application\UI;
 abstract class BasePresenter extends UI\Presenter
 {
 
+	/** @var string */
+	public $siteName;
+
+
+	public function beforeRender()
+	{
+		parent::beforeRender();
+
+		$this->template->siteName = $this->siteName;
+	}
+
 }

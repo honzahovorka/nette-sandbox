d854f8a7ef006879af72e3b98bb545a23dd2b0d5
Igor Hlina <srigi@srigi.sk>
2014-11-18 21:22:41 +0100

    Remove build process from gulpfile
    Webloader in now handling concatenating & reeving of static assets (pretty all the build process was doing)
    
    Also removed that stupid copying of whole app into build folder (not needed too)

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,6 @@
 var del         = require('del');
 var g           = require('gulp-load-plugins')();
 var gulp        = require('gulp');
-var runSequence = require('run-sequence');
-
-var isBuild;
 
 
 // === Develpement ===
@@ -14,7 +11,6 @@ var isBuild;
 gulp.task('clean', function(cb) {
   return del([
       '.sass-cache',
-      'build/*',
       'log/*',
       'temp/btfj.dat',
       'temp/cache',
@@ -50,96 +46,17 @@ gulp.task('watch', function() {
 
 // === Build ===
 
-gulp.task('buildImages', function() {
+gulp.task('minifyImages', function() {
   return gulp.src('www/images/**/*.{gif,jpg,jpeg,png,webp}')
     .pipe(g.imagemin({
       optimizationLevel: 7,
       progressive: true,
       interlaced: true,
     }))
-    .pipe(gulp.dest('build/www/images'));
-});
-
-gulp.task('buildWrapper', function() {
-  var assets = g.useref.assets({
-    searchPath: ['build/www', 'www']
-  });
-  var app = g.filter('js/app.js');
-  var wrapper = g.filter('@wrapper.latte');
-
-  return gulp.src('app/templates/@wrapper.latte')
-    .pipe(g.replace(new RegExp('vendor\\/([\\w\\-\\.\\/]+).((js|css))', 'g'), 'vendor/$1.min.$2'))
-    .pipe(g.replace('{$basePath}/', ''))
-    .pipe(assets)   // concatenate assets defined in HTML build blocks
-
-    .pipe(g.rev())
-    .pipe(assets.restore())
-    .pipe(g.useref())
-    .pipe(wrapper)
-    .pipe(g.rename({
-      extname: '.html'
-    }))
-    .pipe(wrapper.restore())
-    .pipe(g.revReplace())
-    .pipe(wrapper)
-    .pipe(g.rename({
-      extname: '.latte'
-    }))
-    .pipe(g.replace(/(href|src)="(\w)/g, '$1="{$baseUrl}/$2'))
-    .pipe(wrapper.restore())
-    .pipe(gulp.dest('build/app/templates'));
-});
-
-gulp.task('copyApp', function() {
-  return gulp.src([
-    'app/**/*',
-    '!app/templates/@layout.latte',
-    'bin/**/*',
-    'log/.*',
-    'temp/**/.*',
-    'vendor/**/*',
-    'www/*.*',
-    'www/.*',
-  ], {base: './'})
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('copyRevedAssets', function() {
-  return gulp.src([
-      'build/app/templates/css/*',
-      'build/app/templates/js/*',
-    ], {base: './build/app/templates'})
-    .pipe(gulp.dest('build/www'));
-});
-
-gulp.task('cleanBuild', function(cb) {
-  return del([
-      'build/app/templates/css',
-      'build/app/templates/js',
-    ], cb);
-});
-
-gulp.task('jsMaps', function() {
-  return gulp.src([
-      'www/vendor/jquery/dist/jquery.min.map',
-    ])
-    .pipe(gulp.dest('build/www/js'));
+    .pipe(gulp.dest('www/images'));
 });
 
 
 // === Main tasks definitions ===
-//
-
-gulp.task('build', function() {
-  isBuild = true;
-
-  return runSequence(
-    'clean',
-    ['buildImages', 'compass', 'copyApp', 'jsMaps'],
-    'buildWrapper',
-    'copyRevedAssets',
-    'cleanBuild'
-  );
-});
 
 gulp.task('default', ['jshint', 'watch']);

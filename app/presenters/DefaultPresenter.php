7d3e7f6c5d1848b44b4f19adc5e9949060f7a261
Honza Hovorka <honza.hovorka@gmail.com>
2015-01-01 15:14:04 +0100

    cleaning

--- app/presenters/DefaultPresenter.php
+++ app/presenters/DefaultPresenter.php
@@ -8,13 +8,4 @@ class DefaultPresenter extends BasePresenter
 	public function renderDefault()
 	{
 	}
-
-
-	/**
-	 * @return UserPortlet
-	 */
-	protected function createComponentUserPortlet()
-	{
-		return new UserPortlet($this->getUser());
-	}
 }
